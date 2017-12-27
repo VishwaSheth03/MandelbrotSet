677c3669d6b4ba3250d75eed33c2d84921112b5e
Vishwa Sheth <vishwasheth03@yahoo.com>
2017-12-27 12:24:25 -0500

    Add files via upload

---
+++ Mandelbrot Set/Fractals Summative/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Fractals_Summative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mandelBrotPictureBox = new System.Windows.Forms.PictureBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mandelBrotPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mandelBrotPictureBox
+            // 
+            this.mandelBrotPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.mandelBrotPictureBox.Name = "mandelBrotPictureBox";
+            this.mandelBrotPictureBox.Size = new System.Drawing.Size(800, 600);
+            this.mandelBrotPictureBox.TabIndex = 0;
+            this.mandelBrotPictureBox.TabStop = false;
+            this.mandelBrotPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.mandelBrotPictureBox_Paint);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(819, 166);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(145, 60);
+            this.generateButton.TabIndex = 1;
+            this.generateButton.Text = "button1";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 645);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.mandelBrotPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mandelBrotPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mandelBrotPictureBox;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ Mandelbrot Set/Fractals Summative/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Fractals_Summative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap mandelBrotBitmap = new Bitmap(800, 600);
+        int maxNum = 2;
+
+        private void mandelBrotPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(mandelBrotBitmap, 0, 0);
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            Graphics mandelBrotGraphics = Graphics.FromImage(mandelBrotBitmap);
+            Complex z = new Complex(0, 0);
+            Complex c = new Complex(0, 0);
+            int height = mandelBrotBitmap.Height;
+            int width = mandelBrotBitmap.Width;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    z = 0;
+                    int i = 0;
+                    c = new Complex((4.0d / width) * x - 2.5d, (-3.0d / height) * y + 1.5d);
+
+                    do
+                    {
+                        z = Complex.Add(Complex.Pow(z, 2), c);
+                        i++;
+                    } while (i <= 100 && Complex.Abs(z) < maxNum);
+
+                    if (i - 1 == 100)
+                        mandelBrotBitmap.SetPixel(x, y, Color.Black);
+                    else if (i - 1 != 100)
+                        mandelBrotBitmap.SetPixel(x, y, Color.Blue);
+                }
+            }
+            mandelBrotPictureBox.Refresh();
+        }
+    }
+}
